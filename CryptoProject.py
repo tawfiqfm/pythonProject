8a040944c44e5dc42e7c254a2df59cca093108e1
Aaron Willisford <64561892+awillisford@users.noreply.github.com>
2020-09-27 01:58:37 -0400

    fixed underscore and space issue
    In function 'encrypt' I created a statement to remove the last comma if the current iteration in the for loop is a space.

--- CryptoProject.py
+++ CryptoProject.py
@@ -1,45 +1,25 @@
 import string
+
+
 def encrypt(s):
     # this function returns the encrypted form of s
     key = int(s[0:2])
     k = s.index('/')
     time = s[0:k]
-    plaintext = s[ k +1:]
+    plaintext = s[k + 1:]
     result = ""
     for c in plaintext:
 
-
         if c == ' ':
             code = '_'
+            result = result[:-1]
 
         else:
-            code = str((('abcdefghijklmnopqrstuvwxyz'.index(c ) +key) % 26)) + ','
+            code = str((('abcdefghijklmnopqrstuvwxyz'.index(c) + key) % 26)) + ','
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        result = result + code
+        result += code
 
     return (time + '/') + result
-
-
-
 
 
 def decrypt(s):
@@ -56,10 +36,9 @@ def decrypt(s):
             code = str((()))
 
 
-
-def main ():
-  # s = "14:00/the ships are in position"
-  # s = "21:30/17,25_21,12,25_12,25,21,24,19_14,9_21,14,14,21,23,5"
+def main():
+    # s = "14:00/the ships are in position"
+    # s = "21:30/17,25_21,12,25_12,25,21,24,19_14,9_21,14,14,21,23,5"
     while True:
         s = input("\nEnter e plain-text or d coded-text or q: ")
         if s[0] == "e":
